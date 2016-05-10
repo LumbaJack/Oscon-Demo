b620014aa3bd3e917f001e410602d709bbe95320
Jack Garcia <jackgarcia77@gmail.com>
2016-05-10 13:06:59 -0500

    latest changes

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 import kivy
-from kivy.uix.floatlayout import FloatLayout
-kivy.require('1.7.0')
+import json
+import requests
 
+kivy.require('1.7.0')
 from kivy.app import App
-from kivy.lang import Builder   
 from kivy.event import EventDispatcher
-from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty
+from kivy.uix.floatlayout import FloatLayout
 
 class DataModel(object):
     def __init__(self):
-        self.a = 'An quod ita callida est, ut optime possit architectari voluptates? Quodsi ipsam honestatem undique pertectam atque absolutam. Paria sunt igitur. Quorum sine causa fieri nihil putandum est. Ad corpus diceres pertinere-, sed ea, quae dixi, ad corpusne refers? Quis est, qui non oderit libidinosam, protervam adolescentiam? Illa tamen simplicia, vestra versuta. Non igitur potestis voluptate omnia dirigentes aut tueri aut retinere virtutem. \n\nQuid enim tanto opus est instrumento in optimis artibus comparandis? Graccho, eius fere, aequal? Ergo instituto veterum, quo etiam Stoici utuntur, hinc capiamus exordium. Hoc loco tenere se Triarius non potuit. Perturbationes autem nulla naturae vi commoventur, omniaque ea sunt opiniones ac iudicia levitatis.'
-        self.b = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quantum Aristoxeni ingenium consumptum videmus in musicis? Nam quid possumus facere melius? Laboro autem non sine causa; Apud ceteros autem philosophos, qui quaesivit aliquid, tacet; Duo Reges: constructio interrete. Sed nimis multa. Quo modo autem optimum, si bonum praeterea nullum est? Compensabatur, inquit, cum summis doloribus laetitia.'
+        self.a = 'Press start to populate...'
+        self.b = 'Press start to populate...'
 
 common_data_model = DataModel()
 
@@ -49,17 +49,22 @@ class UI_DataModel(EventDispatcher):
             return ret
         return wrapper
 
-
 class RootWidget(FloatLayout):
     ui_data_model = UI_DataModel()
 
     @ui_data_model.update
     def button_press(self, *args):
-        common_data_model.a = 'A'
+        output = json.loads(requests.get('https://70.138.100.58:4443/redfish/' \
+                                         'v1/Telemetry/1/', verify=False).text)
+        output = json.dumps(output, indent=4, sort_keys=True)
+        common_data_model.a = output
 
     @ui_data_model.update
     def button_press2(self, *args):
-        common_data_model.b = 'B'
+        output = json.loads(requests.get('https://70.138.100.58:4443/redfish/' \
+                                         'v1/Telemetry/2/', verify=False).text)
+        output = json.dumps(output, indent=4, sort_keys=True)
+        common_data_model.b = output
 
 class TestApp(App):
     def build(self):
@@ -67,3 +72,4 @@ class TestApp(App):
 
 app = TestApp()
 app.run()
+
